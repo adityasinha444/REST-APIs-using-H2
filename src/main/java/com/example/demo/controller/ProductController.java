e1d9c2add2db6ed225c59a31eb822f2ca803ceac
adityasinha444 <adityasinha.skncoe.it@gmail.com>
2024-11-18 11:14:04 +0530

    added code base

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -52,6 +52,5 @@ public class ProductController {
 		return ResponseEntity.noContent().build();
 	}
 	
-	//Final
 	
 }
--- src/main/java/com/example/demo/service/ProductServiceImpl.java
+++ src/main/java/com/example/demo/service/ProductServiceImpl.java
@@ -11,40 +11,39 @@ import com.example.demo.repository.ProductRepository;
 
 @Service
 public class ProductServiceImpl implements ProductService {
-	
+
 	@Autowired
 	public ProductRepository productRepository;
-	
+
 	@Override
 	public List<Product> getAllProducts() {
 		return productRepository.findAll();
 	}
-	
+
 	@Override
 	public Optional<Product> getProductById(Long id) {
 		return productRepository.findById(id);
 	}
-	
+
 	@Override
 	public Product addProduct(Product product) {
 		return productRepository.save(product);
 	}
-	
+
 	@Override
 	public Product updateProduct(Long id, Product product) {
 		Optional<Product> existingProduct = productRepository.findById(id);
-        if (existingProduct.isPresent()) {
-            Product updatedProduct = existingProduct.get();
-            updatedProduct.setName(product.getName());
-            updatedProduct.setPrice(product.getPrice());
-            return productRepository.save(updatedProduct);
-        }
-        return null;
+		if (existingProduct.isPresent()) {
+			Product updatedProduct = existingProduct.get();
+			updatedProduct.setName(product.getName());
+			updatedProduct.setPrice(product.getPrice());
+			return productRepository.save(updatedProduct);
+		}
+		return null;
 	}
-	
+
 	@Override
 	public void deleteProduct(Long id) {
 		productRepository.deleteById(id);
 	}
 }
-
